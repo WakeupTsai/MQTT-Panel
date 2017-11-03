9d11d703b106291894694375eb7ebc1682564a6f
WakeupTsai <c20712983@gmail.com>
2017-11-03 12:54:39 +0800

    Replace jgPlot.js with chart.js.

---
+++ public/javascripts/drawChart.js
@@ -0,0 +1,159 @@
+var samples = 20;
+var speed = 250;
+var charts = [];
+
+var mq5Values = [];
+var mq7Values = [];
+var mq131Values = [];
+var mq135Values = [];
+
+mq5Values.length = samples;
+mq7Values.length = samples;
+mq131Values.length = samples;
+mq135Values.length = samples;
+
+mq5Values.fill(0);
+mq7Values.fill(0);
+mq131Values.fill(0);
+mq135Values.fill(0);
+
+var labels = [];
+labels.length = samples;
+labels.fill(0);
+
+function initializeChart() {
+  charts.push(
+    new Chart(document.getElementById("mq5Chart"), {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: mq5Values,
+          backgroundColor: 'rgba(255, 99, 132, 0.1)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 2,
+          lineTension: 0.25,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: {
+          duration: speed * 1.5,
+          easing: 'linear'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    }),
+    new Chart(document.getElementById("mq7Chart"), {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: mq7Values,
+          backgroundColor: 'rgba(255, 99, 132, 0.1)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 2,
+          lineTension: 0.25,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: {
+          duration: speed * 1.5,
+          easing: 'linear'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    }),
+    new Chart(document.getElementById("mq131Chart"), {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: mq131Values,
+          backgroundColor: 'rgba(255, 99, 132, 0.1)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 2,
+          lineTension: 0.25,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: {
+          duration: speed * 1.5,
+          easing: 'linear'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    }),
+    new Chart(document.getElementById("mq135Chart"), {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: mq135Values,
+          backgroundColor: 'rgba(255, 99, 132, 0.1)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 2,
+          lineTension: 0.25,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: {
+          duration: speed * 1.5,
+          easing: 'linear'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    })
+  );
+}
--- public/javascripts/sensor-data.js
+++ public/javascripts/sensor-data.js
@@ -4,302 +4,141 @@ var mq131 = new Array();
 var mq135 = new Array();
 var headCount = new Array();
 var socket = io.connect();
-//var socket = io.connect('http://localhost:5000');
-    socket.on('connect', function () {
-        socket.on('mqtt', function (msg) {
-            // iot-1/d/b827ebdf52bd/evt/pollution_air_mq135/json
-            var message = msg.topic.split('/');
 
-            var sensor = message[4];
+socket.on('connect', function () {
+  socket.on('mqtt', function (msg) {
+    var message = msg.topic.split('/');
+    var sensor = message[4];
 
-            console.log('message: '+message);
-            console.log('sensor: '+sensor);
-            //console.log(String.fromCharCode.apply(null, new Uint8Array(msg.payload)));
+    //console.log('message: '+message);
+    //console.log('sensor: '+sensor);
+    //console.log(String.fromCharCode.apply(null, new Uint8Array(msg.payload)));
 
-            var payload = String.fromCharCode.apply(null, new Uint8Array(msg.payload));
+    var payload = String.fromCharCode.apply(null, new Uint8Array(msg.payload));
 
+    $('#topic').html(msg.topic);
+    $('#message').html(msg.topic + ', ' + payload);
+    var obj = JSON.parse(payload);
+    payload = obj.d.value;
+    //console.log('payload: '+payload);
 
-            var timestamp = Math.round((new Date()).getTime() / 1000);
+    switch (sensor) {
 
-            $('#topic').html(msg.topic);
-            $('#message').html(msg.topic + ', ' + payload);
-            var obj = JSON.parse(payload);
-            payload = obj.d.value;
-            console.log('payload: '+payload);
+      case 'audio':
+        $('#audio_result').html('(Sound: ' + payload + ')');
+        $('#sound').text(payload);
+        $('#sound').removeClass('label-danger').addClass('label-success');
 
-            switch (sensor) {
+        break;
 
-                case 'audio':
-                    $('#audio_result').html('(Sound: ' + payload + ')');
+      case 'yolo':
+        $('#yolo_result').html('(Object: ' + payload + ')');
+        $('#object').text(payload);
+        $('#object').removeClass('label-danger').addClass('label-success');
 
-                    $('#sound').text(payload);
-                    $('#sound').removeClass('label-danger').addClass('label-success');
+        break;
 
-                    break;
-
-                case 'yolo':
-                    $('#yolo_result').html('(Object: ' + payload + ')');
-
-                    $('#object').text(payload);
-                    $('#object').removeClass('label-danger').addClass('label-success');
-
-                    break;
-
-                case 'motion':
-
-                    $('#pir_value').html('(Sense value: ' + payload + ')');
-                    console.log("which"+message[2])
-                    if (payload == '0') {
-                      switch (message[2]){
-                        case '0242ac100302':
-                          $('#pir3').text('Nothing');
-                          $('#pir3').removeClass('label-danger').addClass('label-success');
-                          break;
-                        case '0242ac101202':
-                          $('#pir2').text('Nothing');
-                          $('#pir2').removeClass('label-danger').addClass('label-success');
-                          break;
-                        case '0242ac106002':
-                          $('#pir1').text('Nothing');
-                          $('#pir1').removeClass('label-danger').addClass('label-success');
-                          break;
-                      }
-                    } else {
-                      switch (message[2]){
-                        case '0242ac100302':
-                          $('#pir3').text('Motion detected');
-                          $('#pir3').removeClass('label-success').addClass('label-danger');
-                          break;
-                        case '0242ac101202':
-                          $('#pir2').text('Motion detected');
-                          $('#pir2').removeClass('label-success').addClass('label-danger');
-                          break;
-                        case '0242ac106002':
-                          $('#pir1').text('Motion detected');
-                          $('#pir1').removeClass('label-success').addClass('label-danger');
-                          break;
-                        }
-                    }
-                    break;
-
-                case 'pollution_air_mq5':
-                    var mq5_value = (parseFloat(payload))+"";
-                    $('#mq5Sensor').html('(Sensor value: ' + mq5_value + ')');
-                    $('#mq5Label').text(mq5_value);
-                    $('#mq5Label').removeClass('').addClass('label-default');
-
-                    var entry = new Array();
-                    entry.push(timestamp);
-                    entry.push(parseFloat(payload));
-                    mq5.push(entry);
-                    // Show only 20 values
-                    if (mq5.length >= 20) {
-                        mq5.shift()
-                    }
-
-                    var mq5Plot = $.jqplot ('mq5Chart', [mq5], {
-                        axesDefaults: {
-                            labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-                            tickOptions: {
-                                showMark: false,
-                                showGridline: false,
-                                show: false,
-                                showLabel: false,
-                            }
-                          },
-                        grid: {
-                            gridLineColor: '#FFFFFF',
-                            borderWidth: 0,
-                            shadow: false,
-                        },
-                        seriesDefaults: {
-                            rendererOptions: {
-                                smooth: true
-                            },
-                            showMarker: false,
-                            lineWidth: 2,
-                          },
-                          axes: {
-                            xaxis: {
-                              renderer:$.jqplot.DateAxisRenderer,
-                              tickOptions:{
-                                formatString:'%T'
-                              },
-                              pad: 0
-                            },
-                            yaxis: {
-                                tickOptions:{
-                                    formatString: '%.1f'
-                                }
-                            }
-                        }
-                    });
-                    break;
-
-                case 'pollution_air_mq7':
-                    var mq7_value = (parseFloat(payload))+"";
-                    $('#mq7Sensor').html('(Sensor value: ' + mq7_value + ')');
-                    $('#mq7Label').text(mq7_value);
-                    $('#mq7Label').removeClass('').addClass('label-default');
-
-                    var entry = new Array();
-                    entry.push(timestamp);
-                    entry.push(parseFloat(payload));
-                    mq7.push(entry);
-                    // Show only 20 values
-                    if (mq7.length >= 20) {
-                        mq7.shift()
-                    }
-
-                    var mq7Plot = $.jqplot ('mq7Chart', [mq7], {
-                        axesDefaults: {
-                            labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-                            tickOptions: {
-                                showMark: false,
-                                showGridline: false,
-                                show: false,
-                                showLabel: false,
-                            }
-                          },
-                        grid: {
-                            gridLineColor: '#FFFFFF',
-                            borderWidth: 0,
-                            shadow: false,
-                        },
-                        seriesDefaults: {
-                            rendererOptions: {
-                                smooth: true
-                            },
-                            showMarker: false,
-                            lineWidth: 2,
-                          },
-                          axes: {
-                            xaxis: {
-                              renderer:$.jqplot.DateAxisRenderer,
-                              tickOptions:{
-                                formatString:'%T'
-                              },
-                              pad: 0
-                            },
-                            yaxis: {
-                                tickOptions:{
-                                    formatString: '%.1f'
-                                }
-                            }
-                        }
-                    });
-                    break;
-
-                case 'pollution_air_mq131':
-                        var mq131_value = (parseFloat(payload))+"";
-                        $('#mq131Sensor').html('(Sensor value: ' + mq131_value + ')');
-                        $('#mq131Label').text(mq131_value);
-                        $('#mq131Label').removeClass('').addClass('label-default');
-
-                        var entry = new Array();
-                        entry.push(timestamp);
-                        entry.push(parseFloat(payload));
-                        mq131.push(entry);
-                        // Show only 20 values
-                        if (mq131.length >= 20) {
-                            mq131.shift()
-                        }
-
-                        var mq131Plot = $.jqplot ('mq131Chart', [mq131], {
-                            axesDefaults: {
-                                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-                                tickOptions: {
-                                    showMark: false,
-                                    showGridline: false,
-                                    show: false,
-                                    showLabel: false,
-                                }
-                              },
-                            grid: {
-                                gridLineColor: '#FFFFFF',
-                                borderWidth: 0,
-                                shadow: false,
-                            },
-                            seriesDefaults: {
-                                rendererOptions: {
-                                    smooth: true
-                                },
-                                showMarker: false,
-                                lineWidth: 2,
-                              },
-                              axes: {
-                                xaxis: {
-                                  renderer:$.jqplot.DateAxisRenderer,
-                                  tickOptions:{
-                                    formatString:'%T'
-                                  },
-                                  pad: 0
-                                },
-                                yaxis: {
-                                    tickOptions:{
-                                        formatString: '%.1f'
-                                    }
-                                }
-                            }
-                        });
-                        break;
-
-                case 'pollution_air_mq135':
-                        var mq135_value = (parseFloat(payload))+"";
-                        $('#mq135Sensor').html('(Sensor value: ' + mq135_value + ')');
-                        $('#mq135Label').text(mq135_value);
-                        $('#mq135Label').removeClass('').addClass('label-default');
-
-                        var entry = new Array();
-                        entry.push(timestamp);
-                        entry.push(parseFloat(payload));
-                        mq135.push(entry);
-                        // Show only 20 values
-                        if (mq135.length >= 20) {
-                            mq135.shift()
-                        }
-
-                        var mq135Plot = $.jqplot ('mq135Chart', [mq135], {
-                            axesDefaults: {
-                                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-                                tickOptions: {
-                                    showMark: false,
-                                    showGridline: false,
-                                    show: false,
-                                    showLabel: false,
-                                }
-                              },
-                            grid: {
-                                gridLineColor: '#FFFFFF',
-                                borderWidth: 0,
-                                shadow: false,
-                            },
-                            seriesDefaults: {
-                                rendererOptions: {
-                                    smooth: true
-                                },
-                                showMarker: false,
-                                lineWidth: 2,
-                              },
-                              axes: {
-                                xaxis: {
-                                  renderer:$.jqplot.DateAxisRenderer,
-                                  tickOptions:{
-                                    formatString:'%T'
-                                  },
-                                  pad: 0
-                                },
-                                yaxis: {
-                                    tickOptions:{
-                                        formatString: '%.1f'
-                                    }
-                                }
-                            }
-                        });
-                        break;
-
-                default: console.log('Error: Data do not match the MQTT topic.'); break;
+      case 'motion':
+        $('#pir_value').html('(Sense value: ' + payload + ')');
+        //console.log("which"+message[2])
+        if (payload == '0') {
+          switch (message[2]){
+            case '0242ac100302':
+              $('#pir3').text('Nothing');
+              $('#pir3').removeClass('label-danger').addClass('label-success');
+              break;
+            case '0242ac101202':
+              $('#pir2').text('Nothing');
+              $('#pir2').removeClass('label-danger').addClass('label-success');
+              break;
+            case '0242ac106002':
+              $('#pir1').text('Nothing');
+              $('#pir1').removeClass('label-danger').addClass('label-success');
+              break;
+          }
+        } else {
+          switch (message[2]){
+            case '0242ac100302':
+              $('#pir3').text('Motion detected');
+              $('#pir3').removeClass('label-success').addClass('label-danger');
+              break;
+            case '0242ac101202':
+              $('#pir2').text('Motion detected');
+              $('#pir2').removeClass('label-success').addClass('label-danger');
+              break;
+            case '0242ac106002':
+              $('#pir1').text('Motion detected');
+              $('#pir1').removeClass('label-success').addClass('label-danger');
+              break;
             }
- });
- socket.emit('subscribe', {topic : 'iot-1/d/+/evt/#'});
+        }
+        break;
+
+      case 'pollution_air_mq5':
+        var mq5_value = (parseFloat(payload))+"";
+        $('#mq5Sensor').html('(Sensor value: ' + mq5_value + ')');
+        $('#mq5Label').text(mq5_value);
+        $('#mq5Label').removeClass('').addClass('label-default');
+
+        mq5Values.push(mq5_value);
+        mq5Values.shift();
+        charts.forEach(function(chart) { chart.update(); });
+
+        break;
+
+      case 'pollution_air_mq7':
+        var mq7_value = (parseFloat(payload))+"";
+        $('#mq7Sensor').html('(Sensor value: ' + mq7_value + ')');
+        $('#mq7Label').text(mq7_value);
+        $('#mq7Label').removeClass('').addClass('label-default');
+
+        mq7Values.push(mq7_value);
+        mq7Values.shift();
+        charts.forEach(function(chart) { chart.update(); });
+
+        break;
+
+      case 'pollution_air_mq131':
+        var mq131_value = (parseFloat(payload))+"";
+        $('#mq131Sensor').html('(Sensor value: ' + mq131_value + ')');
+        $('#mq131Label').text(mq131_value);
+        $('#mq131Label').removeClass('').addClass('label-default');
+
+        mq131Values.push(mq131_value);
+        mq131Values.shift();
+        charts.forEach(function(chart) { chart.update(); });
+
+        break;
+
+      case 'pollution_air_mq135':
+        var mq135_value = (parseFloat(payload))+"";
+        $('#mq135Sensor').html('(Sensor value: ' + mq135_value + ')');
+        $('#mq135Label').text(mq135_value);
+        $('#mq135Label').removeClass('').addClass('label-default');
+
+        mq135Values.push(mq135_value);
+        mq135Values.shift();
+        charts.forEach(function(chart) { chart.update(); });
+
+        break;
+
+      default: console.log('Error: Data do not match the MQTT topic.'); break;
+    }
+  }
+);
+
+socket.emit('subscribe', {topic : 'iot-1/d/+/evt/#'});
 });
+
+var interval = setInterval(function() {
+    $('img').each(function() {
+        var time = (new Date()).getTime();
+        var oldurl = $(this).prop('src');
+        var newurl = oldurl.split("?")[0]+'?'+ new Date().getTime();
+        $(this).prop('src', newurl);
+    });
+}, 2000);
+
+window.onload = function() {
+  initializeChart();
+};
